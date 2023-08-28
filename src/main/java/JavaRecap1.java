5ab860464a7a25c44b1d21b948af4d4cc62b07a6
Rylie Castell <rylie.castell@gmail.com>
2023-08-28 10:04:47 +0200

    add method for password length

--- src/main/java/JavaRecap1.java
+++ src/main/java/JavaRecap1.java
@@ -2,4 +2,12 @@ public class JavaRecap1 {
     public static void main(String[] args) {
 
     }
+
+    public static boolean isPasswordValid(String password) {
+        int passwordLength = password.length();
+        if(passwordLength >= 8){
+            return true;
+        }
+        return false;
+    }
 }
