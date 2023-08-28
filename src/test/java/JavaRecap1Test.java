34ba38c392abf73a5da4ee49f0ae7a39cdb6a4a6
Rylie Castell <rylie.castell@gmail.com>
2023-08-28 10:15:28 +0200

    add tests for passwords containing numbers

--- src/test/java/JavaRecap1Test.java
+++ src/test/java/JavaRecap1Test.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.Test;
+
+import java.time.chrono.JapaneseChronology;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JavaRecap1Test {
 
     @Test
     void isPasswordValid_whenInputIs8Chars_returnTrue(){
-        String password = "testtest";
+        String password = "test1234";
         boolean actual = JavaRecap1.isPasswordValid(password);
         boolean expected = true;
         assertEquals(expected, actual);
@@ -14,6 +17,22 @@ public class JavaRecap1Test {
     @Test
     void isPasswordValid_whenInputIs7Chars_returnFalse(){
         String password = "test123";
+        boolean actual = JavaRecap1.isPasswordValid(password);
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isPasswordValid_whenInputContainsNumbers_returnTrue(){
+        String password = "1test4you";
+        boolean actual = JavaRecap1.isPasswordValid(password);
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isPasswordValid_whenInputContainsNoNumbers_returnFalse(){
+        String password = "testtest";
         boolean actual = JavaRecap1.isPasswordValid(password);
         boolean expected = false;
         assertEquals(expected, actual);
