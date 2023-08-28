1ff9de44cf368e1620061a0ac2bbbaea22d19322
Rylie Castell <rylie.castell@gmail.com>
2023-08-28 10:02:01 +0200

    add tests for password length

--- src/test/java/JavaRecap1Test.java
+++ src/test/java/JavaRecap1Test.java
@@ -1,2 +1,21 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JavaRecap1Test {
+
+    @Test
+    void isPasswordValid_whenInputIs8Chars_returnTrue(){
+        String password = "testtest";
+        boolean actual = JavaRecap1.isPasswordValid(password);
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isPasswordValid_whenInputIs7Chars_returnFalse(){
+        String password = "test123";
+        boolean actual = JavaRecap1.isPasswordValid(password);
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
 }
